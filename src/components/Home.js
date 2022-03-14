c57cb9fde8d75ae0c0691466db39eec7575d5682
juanMurguia <juancruzmur@gmail.com>
2022-03-14 18:16:43 -0300

    Disney Plus V1

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Home = () => {
+    return (
+        <Container>
+            Home
+        </Container>
+    )
+}
+
+export default Home
+
+const Container = styled.main`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);
+`
